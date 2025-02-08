6ad0c901fbdfb0a2322e604335ac9e19e9d89656
Muhamad Jajuni <Muhamadjajuni98@gmail.com>
2025-02-08 14:51:40 +0700

    exam test

---
+++ src/jawaban/Soal3.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+    nilai: {
+      npm: "1234",
+      namaMahasiswa: "John doe",
+      kodeMataKuliah: "IF-32",
+      nilai: 3.7,
+      tahunAjaran: 2023,
+    },
+    nilai: {
+      npm: "1235",
+      namaMahasiswa: "Jonathan",
+      kodeMataKuliah: "IF-82",
+      nilai: 3.9,
+      tahunAjaran: 2024,
+    },
+  },
+];
+console.log(data[0].nilai.namaMahasiswa);
